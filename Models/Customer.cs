bde1fef72cc5a369d93cf9b4dd51a655748726ae
Trenton <trenton.kuzma@tru-cs.com>
2016-01-27 13:38:45 -0700

    Added EF to Data project

---
+++ Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public Address BillingAddress { get; set; }
+        public Address ShippingAddress { get; set; }
+    }
+}
